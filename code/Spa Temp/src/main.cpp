26694fbddfedb87694bccd0503a4cfc570d1f033
Garth Webb <garth.webb@gmail.com>
2025-04-12 13:26:46 -0700

    Replace existing Logger and WirelessControl libraries with shared modules

--- code/Spa Temp/src/main.cpp
+++ code/Spa Temp/src/main.cpp
@@ -7,96 +7,114 @@
 #include "WirelessControl.h"
 #include "SensorHandler.h"
 #include "InfluxDBHandler.h"
+#include "Telemetry.h"
 
 Logger *LOGGER;
 SensorHandler *SENSOR_HANDLER;
 InfluxDBHandler *INFLUX;
-
-long last_heartbeat_ms = millis();
+Telemetry *TELEMETRY;
 
 void check_for_reset() {
-  esp_reset_reason_t reason = esp_reset_reason();
+    esp_reset_reason_t reason = esp_reset_reason();
 
-  // Log the reset reason, or nothing if no reset was detected
-  switch(reason) {
-      case ESP_RST_UNKNOWN:
-          LOGGER->log_error("Reset reason: Unknown");
-          break;
-      case ESP_RST_POWERON:
-          LOGGER->log_error("Reset reason: Power on");
-          break;
-      case ESP_RST_EXT:
-          LOGGER->log_error("Reset reason: External pin");
-          break;
-      case ESP_RST_SW:    
-          LOGGER->log_error("Reset reason: Software reset");
-          break;
-      case ESP_RST_PANIC:
-          LOGGER->log_error("Reset reason: Panic");
-          break;
-      case ESP_RST_INT_WDT:
-          LOGGER->log_error("Reset reason: Interrupt watchdog");
-          break;
-      case ESP_RST_TASK_WDT:
-          LOGGER->log_error("Reset reason: Task watchdog");
-          break;
-      case ESP_RST_WDT:
-          LOGGER->log_error("Reset reason: Other watchdog");
-          break;
-      case ESP_RST_DEEPSLEEP:
-          LOGGER->log_error("Reset reason: Deep sleep");
-          break;
-      case ESP_RST_BROWNOUT:
-          LOGGER->log_error("Reset reason: Brownout");
-          break;
-      case ESP_RST_SDIO:
-          LOGGER->log_error("Reset reason: SDIO");
-          break;
-  }
+    // Log the reset reason, or nothing if no reset was detected
+    switch(reason) {
+        case ESP_RST_UNKNOWN:
+            LOGGER->log_error("Reset reason: Unknown");
+            break;
+        case ESP_RST_POWERON:
+            LOGGER->log_error("Reset reason: Power on");
+            break;
+        case ESP_RST_EXT:
+            LOGGER->log_error("Reset reason: External pin");
+            break;
+        case ESP_RST_SW:    
+            LOGGER->log_error("Reset reason: Software reset");
+            break;
+        case ESP_RST_PANIC:
+            LOGGER->log_error("Reset reason: Panic");
+            break;
+        case ESP_RST_INT_WDT:
+            LOGGER->log_error("Reset reason: Interrupt watchdog");
+            break;
+        case ESP_RST_TASK_WDT:
+            LOGGER->log_error("Reset reason: Task watchdog");
+            break;
+        case ESP_RST_WDT:
+            LOGGER->log_error("Reset reason: Other watchdog");
+            break;
+        case ESP_RST_DEEPSLEEP:
+            LOGGER->log_error("Reset reason: Deep sleep");
+            break;
+        case ESP_RST_BROWNOUT:
+            LOGGER->log_error("Reset reason: Brownout");
+            break;
+        case ESP_RST_SDIO:
+            LOGGER->log_error("Reset reason: SDIO");
+            break;
+    }
 }
 
 void setup() {
-  // start serial port
-  Serial.begin(115200);
-  Serial.println("Hottub monitor starting up ...");
+    // start serial port
+    Serial.begin(115200);
+    Serial.println("Hottub monitor starting up ...");
 
-  LOGGER = new Logger();
+    LOGGER = new Logger();
+    LOGGER->init(SYSLOG_SERVER, SYSLOG_PORT, HOSTNAME, APP_NAME);
 
-  WirelessControl::init_wifi(WIFI_SSID, WIFI_PASSWORD);
+    WirelessControl::init_wifi(WIFI_SSID, WIFI_PASSWORD, HOSTNAME);
 
-  LOGGER->log("Hottub monitor power cycled, starting up.  Sampling every " + String(SAMPLE_TIME_PERIOD_SEC) + " seconds.");
+    LOGGER->log("Hottub monitor power cycled, starting up.  Sampling every " + String(SAMPLE_TIME_PERIOD_SEC) + " seconds.");
 
-  SENSOR_HANDLER = new SensorHandler();
+    SENSOR_HANDLER = new SensorHandler();
 
-  INFLUX = new InfluxDBHandler(INFLUXDB_URL, INFLUXDB_DB);
-  if (LOG_TO_INFLUX) {
-    INFLUX->enable_logging();
-  } else {
-      INFLUX->disable_logging();
-  }
+    INFLUX = new InfluxDBHandler(INFLUXDB_URL, INFLUXDB_DB);
+    if (LOG_TO_INFLUX) {
+        INFLUX->enable_logging();
+    } else {
+        INFLUX->disable_logging();
+    }
 
-  // Initialize the Watchdog Timer
-  esp_task_wdt_init(WDT_TIMEOUT_SEC, true);
-  // Add the current task to the Watchdog Timer, (the behavior when the task handler == NULL)
-  esp_task_wdt_add(NULL);
+    TELEMETRY = new Telemetry(INFLUXDB_URL, TELEMETRY_DB, HOSTNAME);
+    if (LOG_TELEMETRY) {
+        TELEMETRY->enable();
+    } else {
+        TELEMETRY->disable();
+    }
 
-  // Check for a reset from some prior run
-  check_for_reset();
+    // Initialize the Watchdog Timer
+    esp_task_wdt_init(WDT_TIMEOUT_SEC, true);
+    // Add the current task to the Watchdog Timer, (the behavior when the task handler == NULL)
+    esp_task_wdt_add(NULL);
+
+    // Check for a reset from some prior run
+    check_for_reset();
 }
 
+long last_heartbeat_ms = millis();
+unsigned long last_sample_ms = millis() - SAMPLE_TIME_PERIOD_MS;
+
 void loop() {
-  SENSOR_HANDLER->load_readings();
+    // Handle loading readings, sending to influx and logging telemetry
+    if (millis() - last_sample_ms > SAMPLE_TIME_PERIOD_MS) {
+        SENSOR_HANDLER->load_readings();
 
-  for (auto &sensor : SENSOR_HANDLER->sensors) {
-    INFLUX->add_reading(&sensor);
-  }
+        for (auto &sensor : SENSOR_HANDLER->sensors) {
+            INFLUX->add_reading(&sensor);
+        }
 
-  if (millis() > last_heartbeat_ms + HEARTBEAT_PERIOD_MS) {
-    LOGGER->log("Hottub monitor hearbeat");
-    last_heartbeat_ms = millis();
+        TELEMETRY->report();
+
+        last_sample_ms = millis();
+    }
+
+    if (millis() > last_heartbeat_ms + HEARTBEAT_PERIOD_MS) {
+        LOGGER->log("Hottub monitor hearbeat");
+        last_heartbeat_ms = millis();
+    }
+
+    WirelessControl::monitor();
 
     esp_task_wdt_reset();
-  }
-
-  sleep(SAMPLE_TIME_PERIOD_SEC);
 }
--- code/Spa Temp/src/main.h
+++ code/Spa Temp/src/main.h
@@ -4,14 +4,23 @@
 #include <Arduino.h>
 #include "wifi-info.h"
 
-#define HOSTNAME "hottub-monitor"
+
+#define HOSTNAME "hottub-esp32"
+
+// Syslog server connection info
+#define SYSLOG_SERVER "tigerbackup.local"
+#define SYSLOG_PORT 514
+#define APP_NAME "hottub-monitor"
 
 //-- Influx DB
 //#define INFLUXDB_URL "http://tiger-pi.local:8086"
 #define INFLUXDB_URL "http://192.168.3.88:8086"
 #define INFLUXDB_DB "hottub_monitor"
+#define TELEMETRY_DB "telemetry"
+
 // Quickly enable/disable infux logging
 #define LOG_TO_INFLUX true
+#define LOG_TELEMETRY true
 
 #define ONE_WIRE_BUS D8
 #define ONE_WIRE_SETTLE_MILLIS 500
@@ -22,6 +31,7 @@
 #define HEARTBEAT_PERIOD_SEC 10*60
 #define HEARTBEAT_PERIOD_MS HEARTBEAT_PERIOD_SEC*1000
 
-#define WDT_TIMEOUT_SEC ((int)(HEARTBEAT_PERIOD_SEC*1.5))
+// Set a short watchdog timeout
+#define WDT_TIMEOUT_SEC 20
 
 #endif
